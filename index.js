f79b81e37604cb370c0bace59b57b46448989418
Sasidhar Jonnalagadda <sasidharj1234@gmail.com>
2025-11-18 21:16:42 +0530

    refactor: Add addTaskToTable function
    Added a function called addTaskToTable to avoid code duplication

--- index.js
+++ index.js
@@ -3,40 +3,44 @@ const taskInput = document.getElementById("task-input");
 const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 const table = document.getElementById("taskTable").querySelector("tbody");
 
+function addTaskToTable(taskText) {
+  const row = table.insertRow();
+  const taskCell = row.insertCell();
+  const actionCell = row.insertCell();
+
+  taskCell.textContent = newTask;
+
+  const btn = document.createElement("button");
+  btn.textContent = "Done";
+
+  btn.addEventListener("click", function () {
+    const taskText = row.cells[0].textContent();
+    const taskIndex = tasks.indexOf(taskText);
+
+    if (taskIndex > -1) {
+      tasks.splice(taskIndex, 1);
+    }
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    row.remove();
+  });
+
+  actionCell.appendChild(btn);
+}
+
 addBtn.addEventListener("click", function () {
-  const newTask = taskInput.value;
+  const newTask = taskInput.value.trim();
 
   if (newTask.length <= 3) {
     alert("Length of the task must be at least 3 characters!");
     return;
-  } else {
-    alert("Task added successfully!");
-    tasks.push(newTask.trim());
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    const row = table.insertRow();
-    const taskCell = row.insertCell();
-    const actionCell = row.insertCell();
-
-    taskCell.textContent = newTask;
-
-    const btn = document.createElement("button");
-    btn.textContent = "Done";
-
-    btn.addEventListener("click", function () {
-      const taskText = row.cells[0].textContent();
-      const taskIndex = tasks.indexOf(taskText);
-
-      if (taskIndex > -1) {
-        tasks.splice(taskIndex, 1);
-      }
-
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-      row.remove();
-    });
-
-    actionCell.appendChild(btn);
-
-    taskInput.value = "";
   }
+
+  alert("Task added successfully!");
+  tasks.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  addTaskToTable(newTask);
+
+  taskInput.value = "";
 });
